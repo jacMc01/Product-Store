dfdcbdc33ce8cd4bfb01499b342762b6e56618f2
Jacqueline Mckay H <mcpro@mckay.local>
2025-08-23 11:31:54 -0600

    feat: setup backend

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connnectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(3000, () => {
+  connnectDB();
+  console.log('Server is running on port 3000, http://localhost:3000/');
+});
+
+//1NWUP8Iz28OleWST
+//jacquelinemc
